abc83b2b9d2d6623af284ad331c74fed5f3cf7ab
Asmita <asmita2018fzd@gmail.com>
2025-12-17 00:20:39 +0530

    feat: implement footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { footerLinks } from '../constants'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer>
+      <div className="info">
+        <p> More ways to shop: Visit an Apple Store, call 1-800-MY-APPLE, or find a reseller. </p>
+        <img src='/logo.svg' alt="Apple Logo" />
+      </div>
+
+      <br/>
+
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map(({label,link}) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   )
 }
 
